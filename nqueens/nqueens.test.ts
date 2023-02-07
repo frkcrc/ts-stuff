fd5d289682c163dd342e29098d80ecd3bdf9334b
frkcrc <frkcrc@gmail.com>
2023-02-07 15:53:15 +0100

    Fix strings in nqueens tests

--- nqueens/nqueens.test.ts
+++ nqueens/nqueens.test.ts
@@ -4,19 +4,19 @@ describe('The N-Queen problem', () => {
 
   const solution4q = [...nqueens(4)];
 
-  it(`finds two solutions for N=4`, () => {
+  it('finds two solutions for N=4', () => {
     expect(solution4q.length).toBe(2);
   });
 
-  it(`finds [1,3,0,2] for N=4`, () => {
+  it('finds [1,3,0,2] for N=4', () => {
     expect(solution4q).toContainEqual([1,3,0,2]);
   });
 
-  it(`finds [2,0,3,1] for N=4`, () => {
+  it('finds [2,0,3,1] for N=4', () => {
     expect(solution4q).toContainEqual([2,0,3,1]);
   });
 
-  it(`finds 92 solutions for N=8`, () => {
+  it('finds 92 solutions for N=8', () => {
     const solutions = [...nqueens(8)];
     expect(solutions.length).toBe(92);
   });
